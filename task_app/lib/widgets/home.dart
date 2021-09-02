95071ea9ca2a7277bb6c253f5b07b065479744e8
luizrovaris <luiz_rovaris@hotmail.com>
2021-09-02 22:27:27 +0100

    Adding floating button

--- task_app/lib/widgets/home.dart
+++ task_app/lib/widgets/home.dart
@@ -14,13 +14,26 @@ class HomeState extends State<Home>{
   Widget build(BuildContext context) {
     return Platform.isIOS 
     ? CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(middle: Text('Task App'),),
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Task App'),
+        trailing: TextButton(
+          child: Text('Add'),
+          onPressed: null,
+          ),
+        ),
       child: Text('Home Page'),
     )
     : Scaffold(
       appBar: AppBar(
         title: Text('Task App'),
+        actions: [
+          IconButton(onPressed: null, icon: Icon(Icons.add))
+        ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Icon(Icons.add),
+        ),
       body: Text('Home Page'),
     );
   }
